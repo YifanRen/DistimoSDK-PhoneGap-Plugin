14cb784212508aa7e7566d691ba98d8a3aa540ca
Byunghoon Yoon <bh2yoon@gmail.com>
2014-06-09 20:54:58 -0400

    Restructured code

--- src/bb10/index.js
+++ src/bb10/index.js
@@ -14,125 +14,137 @@
 * limitations under the License.
 */
 
-var distimo;
-
 module.exports = {
-	start: function (success, fail, args, env) {
+	start: function(success, fail, args, env) {
 		var error,
 			result = new PluginResult(args, env);
 
 		args = JSON.parse(decodeURIComponent(args["input"]));
 		if (args.sdkKey) {
-			error = distimo.getInstance().start(args.sdkKey);
+			error = distimo.start(args.sdkKey);
 		}
 
 		if (error) {
 			result.error(error, false);
 		} else {
-			result.ok(distimo.getInstance().debugString(), false);
+			result.ok(distimo.debugString(), false);
 		}
 	},
 
-	version: function (success, fail, args, env) {
+	version: function(success, fail, args, env) {
 		var result = new PluginResult(args, env);
-		result.ok(distimo.getInstance().getVersion(), false);
+		result.ok(distimo.getVersion(), false);
 	},
 
-	logUserRegistered: function (success, fail, args, env) {
-
-	},
-
-	logExternalPurchaseWithCurrency: function (success, fail, args, env) {
+	logUserRegistered: function(success, fail, args, env) {
 
 	},
 
-	logBannerClick: function (success, fail, args, env) {
+	logExternalPurchaseWithCurrency: function(success, fail, args, env) {
 
 	},
 
-	setUserID: function (success, fail, args, env) {
+	logBannerClick: function(success, fail, args, env) {
 
 	},
 
-	openAppLink: function (success, fail, args, env) {
+	setUserID: function(success, fail, args, env) {
+
+	},
+
+	openAppLink: function(success, fail, args, env) {
 
 	}
 };
 
-Distimo = function () {
-	var self = this,
-		hasInstance = false,
-		VERSION = "2.6";
+var distimo = (function() {
+	var VERSION = "2.6";
 
+	var publicKey, privateKey, uuid, imei, eventManager;
 
-	// -- Get Singleton Instance -- //
+	return {
+		debugString: function () {
+			return "new: " + publicKey + " " + privateKey + " " + uuid + " " + imei;
+		},
 
-	self.getInstance = function () {
-		if (!hasInstance) {
-			hasInstance = true;
+		start: function(sdkKey) {
+			// public key, private key
+			if (sdkKey.length <= 12) {
+				return "Please provide a valid SDK Key, you can create one at https://analytics.distimo.com/settings/sdk.";
+			}
+			publicKey = sdkKey.substring(0, 4);
+			privateKey = sdkKey.substring(4);
+			
+			// UUID
+			uuid = Utility.getUUID();
+			if (!uuid) {
+				return "Unable to retrieve device pin.";
+			}
+
+			// device ID
+			imei = Utility.getIMEI();
+			if (!imei) {
+				return "Unable to retrieve IMEI number.";
+			}
+			
+			// event manager
+			eventManager = new EventManager();
+
+			// uncaught exception handler
+			
+			return;
+		},
+
+		getVersion: function() {
+			return VERSION;
+		},
+
+		logUserRegistered: function() {
+			// Get UUID
+			// Then log "UserRegistered"
 		}
-		return self;
 	};
+})();
 
-	// -- Distimo Functions -- //
 
-	var publicKey = "",
-		privateKey = "",
-		uuid = "",
-		imei = "",
-		eventManager = null;
 
-	self.debugString = function () {
-		return "_" + publicKey + " " + privateKey + " " + uuid + " " + imei;
+// - Event Manager -- //
+
+// An event: id, name, params, postData, timestamp, checksum, bundleID, appVersion, sdkVersion	
+
+EventManager = function() {
+	var INITIAL_DELAY = 1000,
+		MAX_DELAY = 32000;
+
+	var delay = INITIAL_DELAY,
+		eventQueue = [];
+
+	function queueEvent(event) {
+		
 	}
 
-	self.start = function (sdkKey) {
-		// public key, private key
-		if (sdkKey.length <= 12) {
-			return "Please provide a valid SDK Key, you can create one at https://analytics.distimo.com/settings/sdk.";
-		}
-		publicKey = sdkKey.substring(0, 4);
-		privateKey = sdkKey.substring(4);
-		
-		// UUID
-		uuid = Utility.getUUID();
-		if (!uuid) {
-			return "Unable to retrieve device pin.";
-		}
+	function storeEvent(event) {
 
-		// device ID
-		imei = Utility.getIMEI();
-		if (!imei) {
-			return "Unable to retrieve IMEI number.";
-		}
-		
-		// event manager
-		eventManager = new EventManager();
-
-		// uncaught exception handler
-		
-		return;
 	}
 
-	self.getVersion = function () {
-		return VERSION;
+	return {
+		logEvent: function(event) {
+			var isApplicationActive = true;
+			if (isApplicationActive) {
+				queueEvent(event);
+			} else {
+				storeEvent(event);
+			}
+		}
 	};
-
-	self.logUserRegistered = function () {
-		// Get UUID
-		// Then log "UserRegistered"
-	}
-
 };
-
-distimo = new Distimo();
 
 
 
 // -- Utility Functions -- //
 
 Utility = {
-	getUUID: function () {
+	getUUID: function() {
 		var str = "";
 		try {
 			str = window.qnx.webplatform.device.devicePin;
@@ -143,7 +155,7 @@ Utility = {
 		return str;
 	},
 
-	getIMEI: function () {
+	getIMEI: function() {
 		var str = "";
 		try {
 			str = window.qnx.webplatform.device.IMEI;
@@ -152,33 +164,5 @@ Utility = {
 			return null;
 		}
 		return str;
-	}
-};
-
-
-
-// - Event -- //
-
-Event = function (args) {
-	// To be filled; requires: id, name, params, postData, timestamp, checksum, bundleID, appVersion, sdkVersion
-};
-
-EventManager = function () {
-	var self = this,
-		hasInstance = false,
-		INITIAL_DELAY = 1000,
-		MAX_DELAY = 32000;
-
-	self.delay = INITIAL_DELAY;
-	self.eventQueue = [];
-
-	// TODO: sort out public & private methods, and apply closure
-
-	self.sendEvent = function (event) {
-
-	}
-
-	self.storeEvent = function (event) {
-
 	}
 };
